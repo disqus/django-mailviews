c853cea950e94805896a9606f900a0b061862177
Brian Grohe <grohe43@gmail.com>
2015-07-16 15:08:09 -0400

    Django 1.8 and python3 compatibilty
    I changed the tests that caused them to fail to fix an encoding issue
    associated with running the tests with python3
    Added try catch to modified imports so it will work
    with older versions of django along with 1.8

--- mailviews/tests/urls.py
+++ mailviews/tests/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import include, patterns, url
+try:
+    from django.conf.urls import include, patterns, url
+except ImportError:
+    from django.conf.urls.defaults import include, patterns, url
 
 from mailviews.previews import autodiscover, site
 
