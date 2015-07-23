ba5269ffcda5d68f8340ae845ff041b670707ba2
Ted Kaemming <ted@kaemming.com>
2015-07-23 15:40:35 -0700

    Improve version specifications.

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 from setuptools import find_packages, setup
 
 
-install_requires = ['django']
-
 setup(name='django-mailviews',
     version='0.6.3',
     url='http://github.com/disqus/django-mailviews/',
@@ -12,7 +10,9 @@ setup(name='django-mailviews',
     description='Class-based mail views for Django',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=install_requires,
+    install_requires=[
+        'Django>=1.3',
+    ],
     test_suite='mailviews.tests.run',
     zip_safe=False,
     license='Apache License 2.0',
