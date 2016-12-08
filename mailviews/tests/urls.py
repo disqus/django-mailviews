86379a80cdecdd74dfd6b39fa6e6533802f08e55
Josh Owen <josh@openfolio.com>
2016-12-07 21:09:50 -0500

    updates for django 1.10

--- mailviews/tests/urls.py
+++ mailviews/tests/urls.py
@@ -1,13 +1,10 @@
-try:
-    from django.conf.urls import include, patterns, url
-except ImportError:
-    from django.conf.urls.defaults import include, patterns, url
+from django.conf.urls import url
 
 from mailviews.previews import autodiscover, site
 
 
 autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(regex=r'', view=site.urls),
-)
+]
